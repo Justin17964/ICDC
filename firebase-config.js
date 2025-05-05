5f2af5d1af28c7ab072d0ba7c3e005608a5dcf1b
Justin17964 <justinsh.qi@gmail.com>
2025-05-05 17:06:43 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,11 @@
-
-// TODO: Replace with your actual Firebase config
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyD4HAgURdf-4kOEziS6W8-KTOZl9_B1aG8",
+  authDomain: "pcdc-2f08e.firebaseapp.com",
+  projectId: "pcdc-2f08e",
+  storageBucket: "pcdc-2f08e.firebasestorage.app",
+  messagingSenderId: "1024625232109",
+  appId: "1:1024625232109:web:7233436e3217a878cda4e4",
+  measurementId: "G-3W73F5B0HZ"
 };
+
 firebase.initializeApp(firebaseConfig);
